9518a658ce590b9300357ff2453edf8d2ca4f8db
kanishk <kanishk5991@gmail.com>
2022-12-20 03:56:28 +0530

    changed pretrained default to false

--- experiments/train/inat/cross-entropy.sh
+++ experiments/train/inat/cross-entropy.sh
@@ -1,2 +1,2 @@
-python main.py --start training --arch custom_resnet18 --batch-size 256 --epochs 100 --loss cross-entropy --optimizer custom_sgd --data inaturalist19-224 --workers 16 --output out/inat/cross-entropy/genus/seed0 --seed 0 --taxonomy genus
+python main.py --start training --arch custom_resnet18 --batch-size 256 --epochs 100 --loss cross-entropy --optimizer custom_sgd --data inaturalist19-224 --workers 16 --output out/inat/cross-entropy/genus/seed0 --seed 0 --taxonomy genus --pretrained
 
